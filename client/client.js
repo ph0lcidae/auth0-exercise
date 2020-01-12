d971f31897de778077cc9a0e085e9dcfa2acfc4f
firinneceartas <firinneceartas@protonmail.com>
2020-01-11 21:21:45 -0500

    get a token automatically instead of having to update expired tokens in config

--- client/client.js
+++ client/client.js
@@ -2,9 +2,15 @@ const ManagementClient = require('auth0').ManagementClient;
 const config = require('../config/config.js');
 
 var auth0Manage = new ManagementClient({
-  token: config.apiToken,
   domain: config.domain,
-  scope: 'read:users update:users'
+  clientId: config.clientID,
+  clientSecret: config.clientSecret,
+  scope: config.scope,
+  audience: config.apiBase,
+  tokenProvider: {
+    enableCache: true,
+    cacheTTLInSeconds: 10
+  }
 });
 
 module.exports = auth0Manage
